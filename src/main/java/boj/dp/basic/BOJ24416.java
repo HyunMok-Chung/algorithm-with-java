ca516dc0e382edc7d27b312acd14ba7262c80c1d
HyunMok-Chung <1021hm@naver.com>
2023-07-22 15:24:38 +0900

    Refactor: Google 컨벤션 적용  - 2023.07.22(토)

--- src/main/java/boj/dp/basic/BOJ24416.java
+++ src/main/java/boj/dp/basic/BOJ24416.java
@@ -10,28 +10,32 @@ public class BOJ24416 {
     private static int cnt2;
     private static int n;
     private static int[] dp;
-    public static int fibo(int a){
-        if(a==1 || a==2){
+
+    public static int fibo(int a) {
+        if (a == 1 || a == 2) {
             cnt1++;
             return 1;
+        } else {
+            return fibo(a - 1) + fibo(a - 2);
         }
-        else
-            return fibo(a-1)+fibo(a-2);
     }
-    public static int fibonacci(int a){
+
+    public static int fibonacci(int a) {
         dp[1] = 1;
         dp[2] = 1;
-        for(int i=3;i<=a;i++){
+        for (int i = 3; i <= a; i++) {
             cnt2++;
-            dp[i] = dp[i-1] + dp[i-2];
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
         return dp[a];
     }
-    public static void main(String[] args) throws IOException{
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        dp = new int[n+1];
-        cnt1 = 0; cnt2 = 0;
+        dp = new int[n + 1];
+        cnt1 = 0;
+        cnt2 = 0;
         fibo(n);
         fibonacci(n);
         StringBuilder sb = new StringBuilder();
